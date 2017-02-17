6874567e41e1360b2360081b6605f6341cdab37b
KiBorg0 <loa92@mail.ru>
2017-02-17 13:35:12 +1000

    изменено отображение connect_info

---
+++ js/info.js
@@ -0,0 +1,47 @@
+﻿/* С какой статьи надо делать выборку из базы при ajax-запросе */
+var startFrom = 10;
+
+$(document).ready(function() {
+	search_info()
+	/* Переменная-флаг для отслеживания того, происходит ли в данный момент ajax-запрос. В самом начале даем ей значение false, т.е. запрос не в процессе выполнения */
+	var inProgress = false;
+
+    /* Используйте вариант $('#more').click(function() для того, чтобы дать пользователю возможность управлять процессом, кликая по кнопке "Дальше" под блоком статей (см. файл index.php) */
+    $(window).scroll(function() {
+
+        /* Если высота окна + высота прокрутки больше или равны высоте всего документа и ajax-запрос в настоящий момент не выполняется, то запускаем ajax-запрос */
+        if($(window).scrollTop() + $(window).height() >= $(document).height() - 200 && !inProgress) {
+	        $.ajax({
+                type:'get',
+	            url: 'view/info.php',
+	            data: {"startFrom" : startFrom},
+            	response:'html',
+	            /* что нужно сделать до отправки запрса */
+	            beforeSend: function() {
+		            /* меняем значение флага на true, т.е. запрос сейчас в процессе выполнения */ 	
+		            inProgress = true;
+	            },
+	            /* возвращаемый результат от сервера */
+                success:function (data) {
+                    $('#info_result').append(data);
+                    inProgress = false;
+                    startFrom += 10;
+                }
+            });
+        }
+    });
+});
+
+function search_info(){
+	startFrom = 10;
+	$('#info_result').html("search...");
+	$.ajax({
+		type:'get',
+		url:'view/info.php',
+		data:{},
+		response:'text',//тип возвращаемого ответа text либо xml
+		success:function (data) {//возвращаемый результат от сервера
+			$('#info_result').html(data);
+		}
+	});
+}
---
+++ view/info.php
@@ -0,0 +1,26 @@
+<?php
+$startFrom = isset($_GET['startFrom']) ? $_GET['startFrom'] : 0;
+
+$mysqligame = new mysqli("localhost", "zisfxloz_base", "W7y9B3r5", "zisfxloz_base");
+$mysqligame->set_charset("utf8");
+
+$mysqligame->real_query("SELECT * FROM url_logs ORDER BY id DESC LIMIT {$startFrom},10");
+$res = $mysqligame->use_result();
+
+echo "<tr>
+<td style = 'width:10%; '>id</td>
+<td style = 'width:40%;'>url</td>
+<td style = 'width:15%;'>информация по реплею</td>
+<td style = 'width:35%;'>информация по действиям запроса</td></tr>";
+
+while ($row = $res->fetch_assoc()) {
+	
+	echo "<tr>
+	<td>" . $row['id'] 							  ."</td>
+	<td>" . str_replace("&", "&amp;", $row['url'])."</td>
+	<td>" . $row['replay_var_dump']			      ."</td>
+	<td>" . $row['apm_calc']					  ."</td></tr>";
+	
+}
+
+?>
